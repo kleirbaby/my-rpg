ea871e4327071dd056bb287917443d67b2afc1bf
kleirbaby <1360848963@qq.com>
2020-03-05 11:59:33 +0800

    my rpg

---
+++ build/CMakeFiles/my-rpg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my-rpg.dir/src/TextureManager.cpp.o"
+  "CMakeFiles/my-rpg.dir/src/Game.cpp.o"
+  "CMakeFiles/my-rpg.dir/src/Player.cpp.o"
+  "CMakeFiles/my-rpg.dir/src/my-rpg.cpp.o"
+  "my-rpg.pdb"
+  "my-rpg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my-rpg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
